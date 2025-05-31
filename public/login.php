3101a22b1c0ff0ba7dc055276a2ebebfca581f0c
Lucas Manera <lucas.manera2017@gmail.com>
2025-05-31 16:09:28 -0300

    feat: finished login page

--- public/login.php
+++ public/login.php
@@ -14,58 +14,30 @@
     <link rel="stylesheet" href="./assets/css/animations.css" />
 </head>
 
-<body>
-    <div id="background" class="animate-gradient flex h-auto min-h-screen items-center justify-center overflow-x-hidden py-10">
-    <div class="relative flex items-center justify-center px-4 sm:px-6 lg:px-8">
-      
-      <div class="bg-gray-300/10 shadow-base-300/20 z-1 w-full space-y-6 rounded-xl p-6 shadow-md sm:min-w-md lg:p-8">
-        <div class="flex items-center justify-center gap-3">
-            <a href="/home" class=" h-[70px] flex items-center">
-                <img class=" ml-3 object-contain w-[200px]" src="./images/Sanquim.png" alt="Singular">
-            </a>
-        </div>
-        <div>
-          <h3 class="text-gray-700 mb-1.5 text-2xl font-semibold text-center">Logar no Singular</h3>
-        </div>
-        <div class="space-y-4">
-          <form class="mb-4 space-y-4" onsubmit="return false;">
-            <div>
-              <label class="label-text text-gray-700" for="userEmail">Email*</label>
-              <input  type="email" placeholder="Enter your email address" class="placeholder:text-gray-500 border px-3 py-1.5 w-full rounded-md bg-gray-300/10 inset-shadow-sm inset-shadow-gray-700/10 border-gray-600" id="userEmail" required />
-            </div>
-            <div>
-              <label class="label-text text-gray-700" for="userPassword">Senha*</label>
-              <div class="border px-3 py-1.5 w-full rounded-md bg-gray-300/10 inset-shadow-sm inset-shadow-gray-700/10 border-gray-600">
-                <input id="userPassword" type="password" placeholder="············" required />
-                <button
-                  type="button"
-                  data-toggle-password='{ "target": "#userPassword" }'
-                  class="flex cursor-pointer"
-                  aria-label="userPassword"
-                >
-                </button>
-                
-              </div>
+                                                <body class="font-inter overflow-hidden">
+    <section class="flex justify-center relative">
+      <img class="animate-[pulse_5s_ease-in_infinite_alternate] w-full" src="./images/backgroundLogin.png" alt="gradient background image" class="w-full h-full object-cover fixed">
+      <div class="mx-auto max-w-lg px-6 lg:px-8 absolute py-20">
+        <img src="./images/Sanquim.png" alt="pagedone logo" width="400" class=" mx-auto lg:mb-11 mb-8 object-cover">
+        <div class="rounded-2xl bg-white shadow-xl">
+          <form action="" class="lg:p-11 p-7 mx-auto">
+            <div class="mb-11">
+              <h1 class="text-gray-900 text-center font-manrope text-3xl font-bold leading-10 mb-2">Login</h1>
               
             </div>
-            <div class="flex items-center justify-between gap-y-2">
-              <div class="flex items-center gap-2">
-                <input type="checkbox" class="checkbox checkbox-primary" id="rememberMe" />
-                <label class="label-text text-base-content/80 p-0 text-base" for="rememberMe">Remember Me</label>
-              </div>
-              <a href="#" class="link link-animated link-primary font-normal">Forgot Password?</a>
-            </div>
-            <button class="bg-[#36918F] btn btn-lg btn-primary btn-block">Sign in to FlyonUI</button>
+            <input type="text" class="w-full h-12 text-gray-900 placeholder:text-gray-400 text-lg font-normal leading-7 rounded-full border-gray-300 border shadow-sm focus:outline-none px-4 mb-6 hover:border-[#E0A400] focus:border-[#E0A400]" placeholder="Username">
+            <input type="text" class="w-full h-12 text-gray-900 placeholder:text-gray-400 text-lg font-normal leading-7 rounded-full border-gray-300 hover:border-[#E0A400] focus:border-[#E0A400] border shadow-sm focus:outline-none px-4 mb-1" placeholder="Password">
+            <a href="javascript:;" class="flex justify-end mb-6">
+              <span  class="text-[#36918f] hover:text-[#E0A400] transition-all duration-500 text-right text-base font-normal leading-6">Esqueceu sua senha?</span>
+            </a>
+            <button class="w-full h-12 cursor-pointer text-white text-center text-base font-semibold leading-6 rounded-full hover:bg-[#E0A400] transition-all duration-500 bg-[#36918f] shadow-sm mb-11">Entrar</button>
+            <a href="javascript:;" class="flex justify-center text-gray-900 text-base font-medium leading-6"> Não possui uma conta? <span class="text-[#36918f] hover:text-[#E0A400] transition-all duration-500 font-semibold pl-3"> Registrar</span>
+            </a>
           </form>
-          <p class="text-base-content/80 mb-4 text-center">
-            New on our platform?
-            <a href="#" class="link link-animated link-primary font-normal">Create an account</a>
-          </p>
-          <div class="divider">or</div>
         </div>
       </div>
-    </div>
-  </div>
+    </section>
+                                            
   
 <script src="./assets/js/flyonui.js"></script>
 <script src="./assets/js/flatpickr.js"></script>
