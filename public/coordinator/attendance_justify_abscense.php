de898c7211ae3899acd714c7be94b369b6db77d9
LucasManera2017 <lucas.manera2017@gmail.com>
2025-06-18 21:23:21 -0300

    feat: finishing coordinator pages

--- public/coordinator/attendance_justify_abscense.php
+++ public/coordinator/attendance_justify_abscense.php
@@ -4,7 +4,146 @@ $tab = "attendance";
 $subtab = "attendance-justify-abscense";
 ?>
 <?php require __DIR__ . "/../../partials/header.php" ?>
-<ul>
-    <li>test</li>
-</ul>
+
+
+<div class="grid grid-cols-2 mt-0 gap-4">
+    <div class="flex flex-col gap-4">
+        <h1 class="font-semibold text-black text-xl mt-6">Justificativa de Falta</h1>
+        <div class="max-h-[600px] flex flex-col flex-wrap gap-4">
+            <div class="p-4 w-full border rounded-md">
+                <div class="flex justify-between w-full">
+                    <h2 class="text-black font-semibold">#001 Justificativa de Falta</h2>
+                    <span class="text-gray-500 font-semibold text-sm">Hoje</span>
+                </div>
+                <article class="text-gray-500 text-sm my-2">
+                    <p>Este é o corpo da justificativa de falta do aluno...</p>
+                </article>
+                <div class="flex items-center justify-between">
+                    <article class="text-gray-500 text-sm flex flex-col">
+                        <strong class="font-medium">Lucas Antônio</strong>
+                        <strong class="font-medium">lucas.antonio@fatec.sp.gov.br</strong>
+                    </article>
+                    <a class="flex items-center gap-2 text-gray-500" href="/">
+                        Ver detalhes
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+                            <path fill="#747171"
+                                d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="p-4 w-full border rounded-md">
+                <div class="flex justify-between w-full">
+                    <h2 class="text-black font-semibold">#002 Justificativa de Falta</h2>
+                    <span class="text-gray-500 font-semibold text-sm">Hoje</span>
+                </div>
+                <article class="text-gray-500 text-sm my-2">
+                    <p>Este é o corpo da justificativa de falta do aluno...</p>
+                </article>
+                <div class="flex items-center justify-between">
+                    <article class="text-gray-500 text-sm flex flex-col">
+                        <strong class="font-medium">Lucas Antônio</strong>
+                        <strong class="font-medium">lucas.antonio@fatec.sp.gov.br</strong>
+                    </article>
+                    <a class="flex items-center gap-2 text-gray-500" href="/">
+                        Ver detalhes
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+                            <path fill="#747171"
+                                d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="p-4 w-full border rounded-md">
+                <div class="flex justify-between w-full">
+                    <h2 class="text-black font-semibold">#001 Justificativa de Falta</h2>
+                    <span class="text-gray-500 font-semibold text-sm">Hoje</span>
+                </div>
+                <article class="text-gray-500 text-sm my-2">
+                    <p>Este é o corpo da justificativa de falta do aluno...</p>
+                </article>
+                <div class="flex items-center justify-between">
+                    <article class="text-gray-500 text-sm flex flex-col">
+                        <strong class="font-medium">Lucas Antônio</strong>
+                        <strong class="font-medium">lucas.antonio@fatec.sp.gov.br</strong>
+                    </article>
+                    <a class="flex items-center gap-2 text-gray-500" href="/">
+                        Ver detalhes
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+                            <path fill="#747171"
+                                d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="p-4 w-full border rounded-md">
+                <div class="flex justify-between w-full">
+                    <h2 class="text-black font-semibold">#001 Justificativa de Falta</h2>
+                    <span class="text-gray-500 font-semibold text-sm">Hoje</span>
+                </div>
+                <article class="text-gray-500 text-sm my-2">
+                    <p>Este é o corpo da justificativa de falta do aluno...</p>
+                </article>
+                <div class="flex items-center justify-between">
+                    <article class="text-gray-500 text-sm flex flex-col">
+                        <strong class="font-medium">Lucas Antônio</strong>
+                        <strong class="font-medium">lucas.antonio@fatec.sp.gov.br</strong>
+                    </article>
+                    <a class="flex items-center gap-2 text-gray-500" href="/">
+                        Ver detalhes
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+                            <path fill="#747171"
+                                d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="p-4 w-full border rounded-md">
+                <div class="flex justify-between w-full">
+                    <h2 class="text-black font-semibold">#001 Justificativa de Falta</h2>
+                    <span class="text-gray-500 font-semibold text-sm">Hoje</span>
+                </div>
+                <article class="text-gray-500 text-sm my-2">
+                    <p>Este é o corpo da justificativa de falta do aluno...</p>
+                </article>
+                <div class="flex items-center justify-between">
+                    <article class="text-gray-500 text-sm flex flex-col">
+                        <strong class="font-medium">Lucas Antônio</strong>
+                        <strong class="font-medium">lucas.antonio@fatec.sp.gov.br</strong>
+                    </article>
+                    <a class="flex items-center gap-2 text-gray-500" href="/">
+                        Ver detalhes
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+                            <path fill="#747171"
+                                d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="p-4 w-full border rounded-md">
+                <div class="flex justify-between w-full">
+                    <h2 class="text-black font-semibold">#001 Justificativa de Falta</h2>
+                    <span class="text-gray-500 font-semibold text-sm">Hoje</span>
+                </div>
+                <article class="text-gray-500 text-sm my-2">
+                    <p>Este é o corpo da justificativa de falta do aluno...</p>
+                </article>
+                <div class="flex items-center justify-between">
+                    <article class="text-gray-500 text-sm flex flex-col">
+                        <strong class="font-medium">Lucas Antônio</strong>
+                        <strong class="font-medium">lucas.antonio@fatec.sp.gov.br</strong>
+                    </article>
+                    <a class="flex items-center gap-2 text-gray-500" href="/">
+                        Ver detalhes
+                        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
+                            <path fill="#747171"
+                                d="M12.6 12L8.7 8.1q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.6 4.6q.15.15.213.325t.062.375t-.062.375t-.213.325l-4.6 4.6q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 <?php require __DIR__ . "/../../partials/footer.php" ?>
--- public/coordinator/enrollment_enroll_student.php
+++ public/coordinator/enrollment_enroll_student.php
@@ -4,5 +4,50 @@ $tab = "enrollment";
 $subtab = "enroll-student";
 ?>
 <?php require __DIR__ . "/../../partials/header.php" ?>
-<h1>Matricular Aluno!</h1>
+<section>
+    <h1 class="font-semibold text-black text-xl mb-4">Registro de Matrículas</h1>
+    <div class="w-full">
+        <div class="flex flex-wrap gap-4">
+            <div class="w-1/2 text-gray-500">
+                <label class="label-text text-gray-500" for="discipline">Nome</label>
+                <input type="text" placeholder="Fulano de tal"
+                    class="input bg-white text-gray-500 placeholder:text-gray-400 border-gray-300 focus:outline-[#F73C39]"
+                    id="name" />
+            </div>
+            <div class="w-1/2 text-gray-500">
+                <label class="label-text text-gray-500" for="discipline">CPF</label>
+                <input type="text" placeholder="444.444.444-22"
+                    class="input bg-white text-gray-500 placeholder:text-gray-400 border-gray-300 focus:outline-[#F73C39]"
+                    id="name" />
+            </div>
+            <div class="w-1/2 text-gray-500">
+                <label class="label-text text-gray-500" for="discipline">RM</label>
+                <input type="text" placeholder="123.344.32.2"
+                    class="input bg-white text-gray-500 placeholder:text-gray-400 border-gray-300 focus:outline-[#F73C39]"
+                    id="name" />
+            </div>
+            <div class="w-1/2 text-gray-500">
+                <label class="label-text text-gray-500" for="course">Curso</label>
+                <select name="course"
+                    class="bg-white text-gray-500 placeholder:text-gray-400 focus:border-none border-gray-300 focus:outline-[#F73C39] select"
+                    id="course">
+                    <option>Ensino Médio | Pré-vestibulinho</option>
+                    <option>Pré-vestibular</option>
+                    <option>Empreendedorismo</option>
+                </select>
+            </div>
+            <div class="w-1/2 text-gray-500">
+                <label class="label-text text-gray-500" for="classroomgroup">Turma</label>
+                <select name="classroomgroup"
+                    class="bg-white text-gray-500 placeholder:text-gray-400 focus:border-none border-gray-300 focus:outline-[#F73C39] select"
+                    id="classroomgroup">
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                </select>
+            </div>
+        </div>
+
+        <button class="btn mt-4 flex flex-end bg-black text-white w-24">Registrar</button>
+</section>
 <?php require __DIR__ . "/../../partials/footer.php" ?>
